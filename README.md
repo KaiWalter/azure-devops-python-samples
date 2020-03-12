7bd7b52c624edaae633538282154731c1cfccf54
Kai Walter <kai.walter@zeiss.com>
2020-03-12 15:27:02 +0000

    added pushdownParentCompletedState

--- README.md
+++ README.md
@@ -2,10 +2,20 @@
 
 Azure DevOps Python SDK samples and helper scripts
 
+> DISCLAIMER: Use these scripts with utmost care and at your own risk!
+
 ## pushdownParentWorkItemCharacteristics
 
 Take values from a defined set of fields and push values from parent to child e.g. transfer AreaPath or IterationPath from Bug to child Tasks.
 
 ```shell
 ./pushdownParentWorkItemCharacteristics.py '-o' 'https://dev.azure.com/{azure-devops-org}' '-t' '{azure-devops-pat}' '-p' '{azure-devops-project} --parent-type 'Bug' --child-type 'Task' --field-list 'System.AreaPath,System.IterationPath'
+```
+
+## pushdownParentCompletedState
+
+Push parent work items completed state to direct non-completed child work items.
+
+```shell
+./pushdownParentCompletedState.py '-o' 'https://dev.azure.com/{azure-devops-org}' '-t' '{azure-devops-pat}' '-p' '{azure-devops-project} --parent-type 'Feature' --child-type 'Product Backlog Item'
 ```
---
+++ pushdownParentCompletedState.py
@@ -0,0 +1,90 @@
+from azure.devops.connection import Connection
+from azure.devops.credentials import BasicAuthentication
+from azure.devops.v6_0.work_item_tracking.models import Wiql, JsonPatchOperation
+
+import argparse
+import datetime
+import os
+import pprint
+
+
+def emit(msg, *args):
+    print(msg % args)
+
+
+def main():
+
+    # extract arguments
+    parser = argparse.ArgumentParser(
+        description='push characteristics from parent work items to children')
+    parser.add_argument("-o", "--org", required=True, dest="url",
+                        help="Azure DevOps Organization URL")
+    parser.add_argument("-p", "--project", required=True, dest="project",
+                        help="Azure DevOps Project")
+    parser.add_argument("-t", "--pat", required=True, dest="pat",
+                        help="Azure DevOps Personal Access Token")
+    parser.add_argument("--parent-type", required=True, dest="parent_type",
+                        help="work item parent type to filter for (Bug,Feature,...)")
+    parser.add_argument("--child-type", required=True, dest="child_type",
+                        help="work item child type to filter for (Task,Product Backlog Item,...)")
+    parser.add_argument("--age", required=False, dest="age", default=120, type=int,
+                        help="age in days when last change of work item happened")
+    args = parser.parse_args()
+
+    # create a connection to the org
+    credentials = BasicAuthentication('', args.pat)
+    connection = Connection(base_url=args.url, creds=credentials)
+
+    # get a client
+    wit_client = connection.clients.get_work_item_tracking_client()
+
+    wi_types = wit_client.get_work_item_types(args.project)
+
+    parent_completed_states = [s.name for s in [
+        t for t in wi_types if t.name == args.parent_type][0].states if s.category == 'Completed']
+    child_completed_states = [s.name for s in [
+        t for t in wi_types if t.name == args.child_type][0].states if s.category == 'Completed']
+    child_removed_states = [s.name for s in [
+        t for t in wi_types if t.name == args.child_type][0].states if s.category == 'Removed']
+
+    # query relations
+    wiql = Wiql(
+        query=f"""SELECT *
+            FROM workitemLinks
+            WHERE [Source].[System.TeamProject] = '{args.project}'
+            AND [Source].[System.WorkItemType] = '{args.parent_type}'
+            AND [Target].[System.WorkItemType] = '{args.child_type}'
+            AND [Source].[System.ChangedDate] >= @today - {args.age}
+            AND [System.Links.LinkType] = 'Child'
+            MODE (MustContain)
+        """
+    )
+
+    wi_relations = wit_client.query_by_wiql(wiql, top=1000).work_item_relations
+    emit("Results: {0}".format(len(wi_relations)))
+
+    # process relations
+    if wi_relations:
+
+        for wir in wi_relations:
+            if wir.source and wir.target:
+
+                # for each source (parent) / target (child) pair check completed state
+                wis = wit_client.get_work_item(wir.source.id)
+                wit = wit_client.get_work_item(wir.target.id)
+
+                if wis.fields['System.State'] in parent_completed_states:
+                    print(
+                        f"{wir.source.id} ({wis.fields['System.State']}) -> {wir.target.id} ({wit.fields['System.State']})")
+
+                    if not wit.fields['System.State'] in child_completed_states and not wit.fields['System.State'] in child_removed_states:
+                        print(f" =>{child_completed_states[0]}")
+                        operations = [JsonPatchOperation(
+                            op='replace', path=f'/fields/System.State', value=child_completed_states[0])]
+                        resp=wit_client.update_work_item(
+                            document = operations, id = wir.target.id)
+                        print(resp)
+
+
+if __name__ == '__main__':
+    main()
